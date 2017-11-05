3a5a0cb18559ce5a17ed2256c26e77ec9279cd6e
Jéssica Aissa <jessica.aissa@live.com>
2017-11-04 23:24:21 -0200

    Movimento do personagem, primeiro nível
    + Teste de movimento do personagem
    + Import do mapa do primero nível Tiled2Unity
    To Do:
    - Verificar qualidade dos sprites importados
    - Configurar câmera corretamente
    - Ajeitar ordem de renderização dos sprites no mapa

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	private Animator animator;
+	private Rigidbody2D body;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+		body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+			body.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, body.velocity.y);
+			//transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+		
+		if(Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+			body.velocity = new Vector2(body.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
+			//transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+
+		if(Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+			body.velocity = new Vector2(0f, body.velocity.y);
+
+		if(Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+			body.velocity = new Vector2(body.velocity.x, 0f);
+
+		animator.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+		animator.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+	}
+}
