40cdb6b34a4d0df00b70da6197e9f27096796797
raulnhl8 <raul.wagner@live.com>
2017-11-12 21:49:34 -0300

    mudancas na movimentacao
    fiz algumas mudancas no codigo da movimentacao, para deixar mais simples e para evitar que o personagem ande na diagonal. TODO apesar de nao andar na diagonal, o personagem ainda roda a animacao, nao sei como resolver

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,32 +8,29 @@ public class PlayerController : MonoBehaviour {
 	private float speedMultiplier;
 	private Animator animator;
 	private Rigidbody2D body;
+	private float horMove;
+	private float verMove;
 
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator>();
 		body = GetComponent<Rigidbody2D>();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
 			speedMultiplier = 1.5f;
 		else speedMultiplier = 1f;
 
-		if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
-			body.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed * speedMultiplier, body.velocity.y);
-			//transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
-		
-		if(Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
-			body.velocity = new Vector2(body.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed * speedMultiplier);
-			//transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+		verMove = Input.GetAxisRaw("Vertical");
+		horMove = Input.GetAxisRaw("Horizontal");
+		if (Mathf.Abs(verMove) == Mathf.Abs(horMove)) {
+			horMove = 0f;
+			verMove = 0f;
+		}
 
-		if(Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
-			body.velocity = new Vector2(0f, body.velocity.y);
-
-		if(Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
-			body.velocity = new Vector2(body.velocity.x, 0f);
+		transform.Translate (horMove * moveSpeed * Time.deltaTime, verMove * moveSpeed * Time.deltaTime, 0);
 
 		animator.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
 		animator.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
