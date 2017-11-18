0ed4d04797485dfd883b4a4bb4b88700f5634664
raulnhl8 <raul.wagner@live.com>
2017-11-18 13:53:14 -0200

    criacao do script do beta
    cria o script do robo beta com as caracteristicas basicas dele

---
+++ Assets/Scripts/RoboBeta.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robo : MonoBehaviour {
+    public int vida;
+    public int inteligencia;
+    public int velocidade;
+    public int ataque;
+    public int defesa;
+
+    private int curVida;
+    private int curInteligencia;
+    private int curVelocidade;
+    private int curAtaque;
+    private int curDefesa;
+
+    private void Awake()
+    {
+        curVida = vida;
+        curInteligencia = inteligencia;
+        curVelocidade = velocidade;
+        curAtaque = ataque;
+        curDefesa = defesa;
+    }
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log(curVida);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
