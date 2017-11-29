0cef4b063f60e9e297fbc2d4475187f0cb201d86
raulnhl8 <raul.wagner@live.com>
2017-11-28 22:45:17 -0200

    Sistema de combate basico
    Implementa o sistema de combate basico.
    TODO
    Criar os assets da cena de combate, Criar os diferentes tipos de inimigo, Fazer integracao com o player.

---
+++ Assets/Scripts/BattleScripts/BattleController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleController : MonoBehaviour {
+
+    //public Button basicAttackButton;
+    public Enemy enemy;
+
+    private void Start()
+    {
+        enemy = GetComponent<Enemy>();
+    }
+
+    public void BasicAttack()
+    {
+        enemy.TakeDamage(1);
+    }
+
+}
+
---
+++ Assets/Scripts/BattleScripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour {
+
+    public Text HPText;
+    public int maxHP = 10;
+    public int curHP = 10;
+
+    public void TakeDamage(int damage)
+    {
+        curHP -= damage;
+        if (curHP <= 0)
+        {
+            HPText.text = " " + 0;
+            //TODO kill enemy
+        }
+        else
+        {
+            HPText.text = " " + curHP;
+        }
+
+    }
+}
