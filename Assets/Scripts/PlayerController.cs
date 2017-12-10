f73e4d6a58846f43cb13b8ffc9af87193e7ea65a
raulnhl8 <raul.wagner@live.com>
2017-12-10 10:08:12 -0200

    Correcao do projeto
    Correcao do projeto

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : Utilities
+{
 
 	public float moveSpeed;
 	private float speedMultiplier;
@@ -13,6 +14,59 @@ public class PlayerController : MonoBehaviour {
     private bool pausado = false;
     private bool controle = false;
     public float altura;
+
+    public GameObject screen;
+
+    void Start()
+    {
+        StartCoroutine(FadeOut(screen));
+        animator = GetComponent<Animator>();
+        body = GetComponent<Rigidbody2D>();
+        BGM = GetComponentInChildren<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pausa();
+        mudo();
+
+        verMove = Input.GetAxisRaw("Vertical");
+        horMove = Input.GetAxisRaw("Horizontal");
+        if (Mathf.Abs(verMove) == Mathf.Abs(horMove))
+        {
+            horMove = 0f;
+            verMove = 0f;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            speedMultiplier = 1.5f;
+        else speedMultiplier = 1f;
+
+        body.velocity = new Vector2(horMove * moveSpeed * speedMultiplier, verMove * moveSpeed * speedMultiplier);
+
+        if (controle == false)
+        {
+            animator.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+            animator.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        }
+
+        // Abre a cena de customizacao se estiver dentro do laboratorio
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (transform.position.x > 19 &&
+                transform.position.x < 23 &&
+                transform.position.y > -49.4 &&
+                transform.position.y < -39)
+                StartCoroutine(FadeIn(screen, "Customization"));
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            StartCoroutine(FadeIn(screen, "BattleScene"));
+        }
+
+    }
 
     void pausa()
     {
@@ -47,61 +101,7 @@ public class PlayerController : MonoBehaviour {
 
     private float verMove, horMove;
 
-	// Use this for initialization
-	void Start () {
-		animator = GetComponent<Animator>();
-		body = GetComponent<Rigidbody2D>();
-        BGM = GetComponentInChildren<AudioSource>();
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        pausa();
-        mudo();
 
-        verMove = Input.GetAxisRaw("Vertical");
-        horMove = Input.GetAxisRaw("Horizontal");
-        if (Mathf.Abs(verMove) == Mathf.Abs(horMove)) {
-            horMove = 0f;
-            verMove = 0f;
-        }
-
-        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
-			speedMultiplier = 1.5f;
-		else speedMultiplier = 1f;
-
-        body.velocity = new Vector2(horMove * moveSpeed * speedMultiplier, verMove * moveSpeed * speedMultiplier);
-        /*
-        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
-			body.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed * speedMultiplier, body.velocity.y);
-
-
-        if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
-            body.velocity = new Vector2(0f, body.velocity.y);
-
-        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
-			body.velocity = new Vector2(body.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed * speedMultiplier);
-			
-
-		if(Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
-			body.velocity = new Vector2(body.velocity.x, 0f);
-            */
-        if (controle == false)
-        {
-            animator.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
-            animator.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
-        }
-        //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
-
-        // Abre a cena de customizacao se estiver dentro do laboratorio
-        if (Input.GetKeyDown(KeyCode.C)) {
-            if (transform.position.x > 19 &&
-                transform.position.x < 23 &&
-                transform.position.y > -49.4 &&
-                transform.position.y < -39)
-            SceneManager.LoadScene("Customization");
-        }
-    }
 
     void OnTriggerStay2D(Collider2D other)
     {
